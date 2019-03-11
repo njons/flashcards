698979ecef8979ea28116e423e651a28d7c30659
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2019-03-11 14:14:03 +0100

    adds /hello route

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 // require the express module and assign it to a variable called express
 const express = require("express");
-
 // the express function returns an express application, assign it a variable called app
 const app = express();
 
-// add routes to teh express server
-app.get("/"); // home route
+// ROUTES OF THE EXPRESS SERVER
+// home route
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+// hello route
+app.get("/hello", (req, res) => {
+  res.send("<h1>Welcome!</h1></br><p>Hello User!</p>");
+});
 
-// set up the server with the .listen() method by giving it a port number
+// START SERVER: set up the server with the .listen() method by giving it a port number
 app.listen(3000, () => {
   console.log(`server is up and running on port 3000`);
 });
