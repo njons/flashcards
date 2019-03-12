d8e4ce0ab6d75fb849fd0a5540f3e5baa93cb4c5
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2019-03-12 11:02:51 +0100

    ads bodyparser and successfully passes input to server

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 // require the express module and assign it to a variable called express
 const express = require("express");
-
+//require body parser to be able to pass data to the server
+const bodyParser = require("body-parser");
 // the express function returns an express application, assign it a variable called app
 const app = express();
 
 // SET THE VIEW ENGINE TO PUG (if you nest the app.js file you will need to point it to the node_modules)
 app.set("view engine", "pug");
 
+// USE MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // ROUTES OF THE EXPRESS SERVER
 // home route, renders the template file named index
 app.get("/", (req, res) => {
   res.render("index");
 });
-// hello route, for testing
+// hello route
 app.get("/hello", (req, res) => {
-  res.send("<h1>Welcome!</h1></br><p>Hello User!</p>");
+  res.render("hello");
 });
+app.post("/hello", (req, res) => {
+  console.dir(req.body);
+  res.render("hello");
+});
+
 // card route for showing cards
 app.get("/cards", (req, res) => {
   res.render("card", {
