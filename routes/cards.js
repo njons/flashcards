53b07e71fa3da8b7b4b7ad259b095b9b9c1f45e3
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2019-03-13 10:35:29 +0100

    adds id and querystring to url in cardRoutes

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const { data } = require("../data/flashcardData.json");
+const { cards } = data;
 
 // cards route: GET
-router.get("/", (req, res) => {
-  res.render("card", {
-    prompt: "This is a question",
-    hint: "this is a clever hint"
-  });
+router.get("/:id", (req, res) => {
+  const { side } = req.query;
+  const { id } = req.params;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+  const templateData = { text, hint };
+  res.render("card", templateData);
 });
 
 module.exports = router;
